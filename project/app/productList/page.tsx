fbe2aee4f50fa158ac0bab8326e0de842de32aa1
zosuzss <seiya0813mana@icloud.com>
2024-07-03 10:00:48 +0900

    first commit

---
+++ project/app/productList/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const pageproduct = () =>{
+    return <div>aiueo</div>;
+};
+
+export default pageproduct;
--- project/feature/signUp/utils/index.ts
+++
@@ -1,5 +0,0 @@
-// コンポーネントに書くと鬱陶しい関数をここに書く
-export const getUser = async (email: string) => {
-  await fetch("");
-  return email;
-};
